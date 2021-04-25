051155e666c6652effe6b9413a60eb3e1aabd629
Zymer Gashi <zymer.gashii@outlook.com>
2021-04-25 16:24:43 +0200

    Created the project references from the main project to the other ones, DataAccess and Models

--- DailyMenu.Models/ViewModels/ErrorViewModel.cs
+++ DailyMenu.Models/ViewModels/ErrorViewModel.cs
@@ -1,6 +1,6 @@
 using System;
 
-namespace DailyMenu.Models
+namespace DailyMenu.Models.ViewModels
 {
     public class ErrorViewModel
     {
--- DailyMenu/Controllers/HomeController.cs
+++ DailyMenu/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using DailyMenu.Models;
+﻿using DailyMenu.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
