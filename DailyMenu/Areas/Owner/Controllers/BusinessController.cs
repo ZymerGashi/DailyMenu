6978a6f6a3f4bde6a627a58e68db2b5f59970555
Zymer Gashi <zymer.gashii@outlook.com>
2021-05-18 19:00:06 +0200

    Added the RegisterBusiness controller which will be used from the owners to register their businesses. Modified the Index view of this controller to show the form which will be used to enter the business data for register purposes. Also on the side of the form, I have added the map which on page load will show the current logged in owner location. In order to set the business map location in the business register form, I have added a click event on the map which will update the Map Coordinates field in the form with the exact coordinates of the business once clicked in the business location
    Added the Business controller and the Index view. These will be used to show the businesses that are already registered for the logged in Owner. Currently I have just added the layout of the view and the logic needs to be applied in the comming commits

---
+++ DailyMenu/Areas/Owner/Controllers/BusinessController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DailyMenu.Areas.Owner.Controllers
+{
+    [Area("Owner")]
+    public class BusinessController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+
+
+
