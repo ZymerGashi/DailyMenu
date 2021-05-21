378ca60d726be9f55de14e215a21dbd5370eab07
Zymer Gashi <zymer.gashii@outlook.com>
2021-05-21 15:56:50 +0200

    Added the logic to set the logged in User as the Owner of the new created businesses

--- DailyMenu/Areas/Owner/Controllers/RegisterBusinessController.cs
+++ DailyMenu/Areas/Owner/Controllers/RegisterBusinessController.cs
@@ -2,11 +2,13 @@
 using DailyMenu.DataAccess.Repository;
 using DailyMenu.DataAccess.Repository.IRepository;
 using DailyMenu.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace DailyMenu.Areas.Owner.Controllers
@@ -16,20 +18,17 @@ namespace DailyMenu.Areas.Owner.Controllers
     {
 
         private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        private readonly ApplicationDbContext _db;
 
-        public DbSet<Business> dbSet;
-
-        public RegisterBusinessController(IUnitOfWork unitOfWork, ApplicationDbContext db)
+        public RegisterBusinessController(IUnitOfWork unitOfWork, ApplicationDbContext db, UserManager<IdentityUser> userManager)
         {
             _unitOfWork = unitOfWork;
-            _db = db;
-
-            dbSet = db.Set<Business>();
+            _userManager = userManager;
         
         }
 
+        public ClaimsPrincipal ClaimsType { get; private set; }
 
         public IActionResult Index()
         {
@@ -40,6 +39,8 @@ namespace DailyMenu.Areas.Owner.Controllers
         [HttpPost]
         public IActionResult Post(Business business)
         {
+
+             business.OwnerId = _userManager.GetUserId(HttpContext.User);
                 _unitOfWork.Business.Add(business);
                 _unitOfWork.Save();
 
