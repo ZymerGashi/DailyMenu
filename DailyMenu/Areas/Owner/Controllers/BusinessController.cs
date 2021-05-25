ada4fca8a989ab76c52b348fd692069977f282c9
Zymer Gashi <zymer.gashii@outlook.com>
2021-05-25 17:19:13 +0200

    Added the table which shows all the businesses registered from the logged in owner

--- DailyMenu/Areas/Owner/Controllers/BusinessController.cs
+++ DailyMenu/Areas/Owner/Controllers/BusinessController.cs
@@ -1,4 +1,6 @@
-﻿using DailyMenu.Models;
+﻿using DailyMenu.DataAccess.Repository.IRepository;
+using DailyMenu.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -10,9 +12,24 @@ namespace DailyMenu.Areas.Owner.Controllers
     [Area("Owner")]
     public class BusinessController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public BusinessController(IUnitOfWork unitOfWork,UserManager<IdentityUser> userManager)
+        {
+            _unitOfWork = unitOfWork;
+            _userManager = userManager;
+        }
+
+
         public IActionResult Index()
         {
-            return View();
+            var loggedInUserID = _userManager.GetUserId(HttpContext.User);
+
+
+            IEnumerable<Business> registeredBusinesses = _unitOfWork.Business.GetAll(b => b.OwnerId == loggedInUserID, includeProperties : "City,Category");
+
+            return View(registeredBusinesses);
         }
 
    
